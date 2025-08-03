26b27099683bdb5e948490fc4eea8ef977bf7d5a
samir rain <teamofsamir@gmail.com>
2025-08-03 18:22:02 +0545

    'update

--- course/src/components/Header.tsx
+++ course/src/components/Header.tsx
@@ -37,6 +37,7 @@ export default function Header() {
                   Courses
                 </NavigationMenuTrigger>
                 
+                
               </NavigationMenuItem>
             </NavigationMenuList>
           </NavigationMenu>
