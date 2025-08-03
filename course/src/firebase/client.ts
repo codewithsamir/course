fb27baded84a57652edf1a16076b2f7c387d62a2
samir rain <teamofsamir@gmail.com>
2025-08-03 18:51:52 +0545

    update

---
+++ course/src/firebase/client.ts
@@ -0,0 +1,22 @@
+import { initializeApp,getApp,getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCftrdQMJfD1mTTPVKebbwql4AnIAmryzY",
+  authDomain: "test-60b1e.firebaseapp.com",
+  projectId: "test-60b1e",
+  storageBucket: "test-60b1e.firebasestorage.app",
+  messagingSenderId: "1021559944747",
+  appId: "1:1021559944747:web:ca9b3a8d5464f16ccd9c6e",
+  measurementId: "G-0BHWFGNHBB"
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+// export const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
